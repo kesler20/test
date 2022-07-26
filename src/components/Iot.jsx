197ddbe4a4f8cd7bf77fee142a21fd3df25aab72
uos-datavisdashboard <uchekesla@gmail.com>
2022-07-26 09:34:16 +0100

    removed secret info

--- src/components/Iot.jsx
+++ src/components/Iot.jsx
@@ -1,7 +1,4 @@
-import React from "react";
-import { Button } from "@material-ui/core";
 import client, { check } from "./mqttProtocol";
-import { useEffect, useState } from "react";
 
 const plotly = window.Plotly;
 
@@ -46,119 +43,6 @@ let config = {
   editable: true,
 };
 
-const initialisePlotData = (data) => {
-  let trend1 = {
-    x: [0],
-    y: [getData(data)[0]],
-    mode: "lines",
-    name: "Trend 1",
-    line: {
-      color: "blue",
-      dash: "dot",
-    },
-  };
-
-  let total1 = {
-    x: [0],
-    y: [getData(data)[1]],
-    mode: "lines",
-    name: "Channel 1",
-    line: {
-      color: "blue",
-      dash: "solid",
-      width: "5",
-    },
-  };
-
-  let trend2 = {
-    x: [0],
-    y: [getData(data)[2]],
-    name: "Trend 2",
-    mode: "lines",
-    line: {
-      color: "red",
-      dash: "dot",
-    },
-  };
-
-  let total2 = {
-    x: [0],
-    y: [getData(data)[3]],
-    mode: "lines",
-    name: "Channel 2",
-    line: {
-      color: "red",
-      dash: "solid",
-      width: "5",
-    },
-  };
-
-  let trend1UpperBoundary = {
-    x: [0],
-    y: [getData(data)[0] + 20],
-    mode: "lines",
-    name: "Trend 1 Upper Bound",
-    line: {
-      color: "rgb(55,128,191)",
-    },
-  };
-
-  let trend1LowerBoundary = {
-    x: [0],
-    y: [getData(data)[0] - 20],
-    mode: "lines",
-    fill: "tonexty",
-    name: "Trend 1 Lower Bound",
-    line: {
-      color: "rgb(55,128,191)",
-    },
-  };
-  let trend2UpperBoundary = {
-    x: [0],
-    y: [getData(data)[2] + 5],
-    mode: "lines",
-    name: "Trend 2 Upper Bound",
-    line: {
-      color: "rgb(254,92,92)",
-    },
-  };
-
-  let trend2LowerBoundary = {
-    x: [0],
-    y: [getData(data)[2] - 5],
-    mode: "lines",
-    fill: "tonexty",
-    name: "Trend 2 Lower Bound",
-    line: {
-      color: "rgb(254,92,92)",
-    },
-  };
-
-  return [
-    trend1,
-    total1,
-    trend2,
-    total2,
-    trend1UpperBoundary,
-    trend1LowerBoundary,
-    trend2UpperBoundary,
-    trend2LowerBoundary,
-  ];
-};
-
-const initialData = {
-  x_value: 2,
-  total_1: 1001.0,
-  total_2: 1079.3520691329636,
-  trend_1: 1000.0,
-  trend_2: 1079.8000000000002,
-};
-
-const Iot = () => {
-  const [clicked, setClicked] = useState(false);
-  const [data, setData] = useState(initialisePlotData(initialData));
-
-  useEffect(() => {
     let cnt = 0;
     plotly.plot("plot", data, layout, config);
 
@@ -232,20 +116,3 @@ const Iot = () => {
         });
       }
     });
-  }, []);
-
-  const toggleClicked = () => {
-    let newClicked = !clicked;
-    setClicked(newClicked);
-  };
-  return (
-    <React.Fragment>
-      <div id="plot"></div>
-      <Button color="primary" onClick={toggleClicked}>
-        Add 1
-      </Button>
-    </React.Fragment>
-  );
-};
-
-export default Iot;
