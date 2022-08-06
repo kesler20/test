9f6faad57f90cf363309e4f48e1326eec5118de9
uos-datavisdashboard <uchekesla@gmail.com>
2022-08-06 01:45:00 +0100

    created a new navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import Logo from "../Logo.jpg";
@@ -19,12 +19,17 @@ const Navbar = () => {
   const renderSideBar = (toRender) => {
     if (toRender) {
       return (
-        <SideBar>
-          <Link to={"./"}>IoT</Link>
-          <Link to={"./userUpload"}>Upload Files</Link>
-          <Link to={"./userAccount"}>Account</Link>
+        <NavbarLinks>
+          <div onClick={() => changeBarView()}>
+            <i className="fa fa-bars" style={{ color: "white" }}></i>
+          </div>
+          <hr />
+          <Link to={"./"}><i className="fa fa-line-chart"></i></Link>
+          <Link to={"./userUpload"}><i className="fa fa-file"></i></Link>
+          <Link to={"./userAccount"}><i className="fa fa-user"></i></Link>
           <Link to={"./test"}>test</Link>
-        </SideBar>
+          <Link to={"./dashboard"}><i className="fa fa-chart-pie"></i></Link>
+        </NavbarLinks>
       );
     } else {
       return "";
@@ -32,112 +37,47 @@ const Navbar = () => {
   };
   return (
     <div>
-      <NavBar>
-        <HamburgerMenu onClick={() => changeBarView()}>
-          <div className="hamburger"></div>
-          <div className="hamburger"></div>
-          <div className="hamburger"></div>
-        </HamburgerMenu>
-        <SiteTitle>
-          <p>JOURNAL</p>
-        </SiteTitle>
-        <Version>
-          <p>version: {process.env.REACT_APP_VERSION}</p>
-        </Version>
-      </NavBar>
-      <hr />
-      {renderSideBar(sideBarView)}
+      <SideBar>
+        <div onClick={() => changeBarView()}>
+          <i className="fa fa-bars" style={{ color: "white" }}></i>
+        </div>
+        <hr />
+        {renderSideBar(sideBarView)}
+      </SideBar>
     </div>
   );
 };
 
-const NavBar = styled.nav`
-  --nav-margins: 100px;
-
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  width: 100%;
-  height: 80px;
-  border: 10px black;
-`;
-
-const HamburgerMenu = styled.div`
-  border: 0.1px solid rgb(186, 202, 183);
-  width: 57px;
-  height: 42px;
-  margin-right: 50px;
-  border-radius: 5px;
-  display: flex;
-  justify-content: space-evenly;
-  flex-direction: column;
-  align-items: center;
-
-  :hover {
-    box-shadow: 1px 1px 1px 10px rgb(245, 255, 229);
-  }
-
-  .hamburger {
-    border-bottom: solid 3px grey;
-    width: 28px;
-  }
-`;
-const SiteTitle = styled.div`
-  font-size: 40px;
-  font-weight: 700;
-  width: 280px;
-  display: flex;
-  justify-content: space-evenly;
-  margin-left: var(--nav-margins);
-
-  p {
-    /* global 94%+ browsers support */
-    background: linear-gradient(
-      90deg,
-      rgba(0, 255, 235, 1) 0%,
-      rgba(7, 58, 187, 1) 100%
-    );
-    -webkit-text-fill-color: transparent;
-    -webkit-background-clip: text;
-    background-clip: text;
-  }
-
-  img {
-    width: 59px;
-    height: 53px;
-    border-radius: 20px;
-  }
-`;
-
 const Link = styled(NavLink)`
   text-decoration: none;
   outline: none;
-  margin-right: 20px;
+  margin: 20px;
   color: rgb(104, 104, 104);
 `;
 
-const Version = styled.div`
-  font-size: 28px;
-  font-weight: 400;
-  display: flex;
-  color: #555555;
-  justify-content: space-evenly;
-  width: 180px;
-  align-items: center;
-  margin-left: var(--nav-margins);
-`;
-
 const SideBar = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 4rem;
+  margin: 0;
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-
-  .nav-link:hover {
-    border-bottom: 1px solid rgb(104, 104, 104);
-  }
+  background-color: black;
+  flex-direction: column;
+  `;
+  
+  const NavbarLinks = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 10rem;
+  margin: 0;
+  display: flex;
+  display: flex;
+  background-color: black;
+  flex-direction: column;
 `;
 
 export default Navbar;
